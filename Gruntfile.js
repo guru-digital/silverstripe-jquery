bd78fb586f7d62506750df1da775818e233fe305
Corey Sewell <corey@gdmedia.tv>
2014-11-19 13:20:11 +1300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt); // load all grunt tasks. Done!
+
+//    grunt.loadNpmTasks('grunt-bower-install-task');
+//    grunt.loadNpmTasks('grunt-bower');
+//    grunt.loadNpmTasks("grunt-contrib-uglify");
+//    require('load-grunt-tasks')(grunt); // load all grunt tasks. Done!
+
+//    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        // Import package manifest
+        pkg: grunt.file.readJSON("package.json"),
+        // Banner definitions
+        bower: {
+            dev: {
+                dest: 'thirdparty/',
+                options: {
+                    stripAffix: true,
+                    expand: true,
+                    keepExpandedHierarchy: false
+                }
+            }
+        },
+        concat: {
+            options: {
+                footer: 'jQuery.migrateMute = true;',
+            },
+            dist: {
+                src: ['thirdparty/jquery/jquery.js'],
+                dest: 'thirdparty/jquery/jquery.js'
+            }
+        },
+        uglify: {
+            jquery: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'thirdparty/jquery/jquery.min.js': ['thirdparty/jquery/jquery.js'],
+                    'thirdparty/jquery-migrate/jquery-migrate.min.js': ['thirdparty/jquery-migrate/jquery-migrate.js']
+                }
+            }
+        }
+    });
+    grunt.registerTask("default", ["bower_install", "bower", "concat", "uglify"]);
+};
