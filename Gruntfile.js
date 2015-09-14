702b3e5b5621247df367668c8541c0d2a3472574
Corey Sewell <corey@gdmedia.tv>
2015-09-14 20:22:01 +1200

    Extend Controller instead of Page_Controller to catch login page as well

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,81 @@
 
 module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt); // load all grunt tasks. Done!
-
-//    grunt.loadNpmTasks('grunt-bower-install-task');
-//    grunt.loadNpmTasks('grunt-bower');
-//    grunt.loadNpmTasks("grunt-contrib-uglify");
-//    require('load-grunt-tasks')(grunt); // load all grunt tasks. Done!
-
-//    require('load-grunt-tasks')(grunt);
-    grunt.initConfig({
-        // Import package manifest
-        pkg: grunt.file.readJSON("package.json"),
-        // Banner definitions
-        bower: {
-            dev: {
-                dest: 'thirdparty/',
-                options: {
-                    stripAffix: true,
-                    expand: true,
-                    keepExpandedHierarchy: false
-                }
-            }
-        },
-        concat: {
-            options: {
-                footer: 'jQuery.migrateMute = true;',
-            },
-            dist: {
-                src: ['thirdparty/jquery/jquery.js'],
-                dest: 'thirdparty/jquery/jquery.js'
-            }
-        },
-        uglify: {
-            jquery: {
-                options: {
-                    sourceMap: true
-                },
-                files: {
-                    'thirdparty/jquery/jquery.min.js': ['thirdparty/jquery/jquery.js'],
-                    'thirdparty/jquery-migrate/jquery-migrate.min.js': ['thirdparty/jquery-migrate/jquery-migrate.js']
-                }
-            }
-        }
-    });
-    grunt.registerTask("default", ["bower_install", "bower", "concat", "uglify"]);
+	require('load-grunt-tasks')(grunt);
+	grunt.initConfig({
+		// Import package manifest
+		pkg: grunt.file.readJSON("package.json"),
+		// Banner definitions
+		bower: {
+			dev: {
+				dest: 'thirdparty/',
+				options: {
+					stripAffix: true,
+					expand: true,
+					keepExpandedHierarchy: false
+				}
+			}
+		},
+		concat: {
+			options: {
+				footer: 'jQuery.migrateMute = true;'
+			},
+			dist: {
+				src: ['thirdparty/jquery/jquery.js'],
+				dest: 'thirdparty/jquery/jquery.js'
+			}
+		},
+		uglify: {
+			jquery: {
+				options: {
+					sourceMap: true
+				},
+				files: {
+					'thirdparty/jquery/jquery.min.js': ['thirdparty/jquery/jquery.js'],
+					'thirdparty/jquery-migrate/jquery-migrate.min.js': ['thirdparty/jquery-migrate/jquery-migrate.js']
+				}
+			}
+		},
+		update_json: {
+			options: {
+				indent: "\t"
+			},
+			bower: {
+				src: "package.json",
+				dest: "bower.json",
+				fields: [
+					"name",
+					"description",
+					"license",
+					"homepage",
+					"keywords",
+					{"authors": ["author"]},
+					"repository",
+					"private"
+				]
+			},
+			composer: {
+				src: "package.json",
+				dest: "composer.json",
+				fields: [
+					{
+						"name": function (src) {
+							return "gdmedia/" + src.name;
+						}
+					},
+					"description",
+					"license",
+					"homepage",
+					"keywords",
+					{"authors": ["author"]},
+					{
+						support: {
+							"issues": "/bugs/url",
+							"email": "/bugs/email"
+						}
+					}
+				]
+			}
+		},
+	});
+	grunt.registerTask("default", ["bower_install", "bower", "concat", "uglify"]);
 };
