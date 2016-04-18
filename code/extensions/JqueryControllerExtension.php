d478bc555737d293067efc9084a0ded81425511e
russell <russell@gurudigital.nz>
2016-04-19 09:56:40 +1200

    Prevent jquery being include on non authenticating calls to security controller

--- code/extensions/JqueryControllerExtension.php
+++ code/extensions/JqueryControllerExtension.php
@@ -1,15 +1,25 @@
 <?php
 
 /**
- *
+ * @property Controller $owner
  */
-class JqueryControllerExtension extends Extension
-{
+class JqueryControllerExtension extends Extension {
 
-    public function onBeforeInit()
-    {
-        if (!is_subclass_of(Controller::curr(), "LeftAndMain")) {
+    public function onBeforeInit() {
+        if (
+                !$this->isCMS() || $this->isAuthentication()
+        ) {
             SilverStripeJquery::requireJquery();
         }
     }
+
+    public function isCMS() {
+        return is_subclass_of($this->owner, "LeftAndMain");
+    }
+
+    public function isAuthentication() {
+        return $this->owner->getRequest()->param("Controller") == "Security" &&
+                in_array($this->owner->getRequest()->param("Action"), array("login", "logout"));
+    }
+
 }
